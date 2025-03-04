2a19e33e627ec242d6f02e24f2edd8c073a6e8c6
brucantacini <brunocantacini.ti@gmail.com>
2025-03-04 16:54:01 -0300

    Criando função para capturar o valor do imput e criar uma nova tarefa.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-// Criação de um array de tarefas inicial
+//Criando de um array de tarefas inicial
 let tarefas = [
     { id: 1, titulo: "Estudar JS", concluida: false }
 ];
+
+// Selecionando elementos do DOM
+const botaoCadastro = document.getElementById("botao-cadastro");
+const botaoFiltrar = document.getElementById("botao-filtrar");
+const listaTarefas = document.getElementById("lista-tarefas");
+const inputTarefa = document.getElementById("idTarefa");
+
+
+// Função para criar uma nova tarefa com alert
+function criarTarefa(titulo, concluida = false) {
+    const novaTarefa = {
+        id: tarefas.length + 1,
+        titulo,
+        concluida
+    };
+
+    tarefas = [...tarefas, novaTarefa]; // Adiciona a nova tarefa ao array
+    renderizarTarefas(); // Atualiza a lista de tarefas
+    alert(`Tarefa "${titulo}" adicionada com sucesso!`); // Exibe o alert
+}
+
